c2388c43a001921f150a6f99a17074509d3c8381
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-05-06 18:54:29 +0530

    orders and users route

--- app.js
+++ app.js
@@ -31,7 +31,24 @@ app.get("/library-3",(req,res)=>{
 app.get("/Welcome",(req,res)=>{
     res.send(`<h1>Welcome ${req.user}</h1>`)
 })
+
+
+app.get("/orders",(req,res)=>{
+    res.send("<h1>Here is the list of all orders.</h1>")
+})
+app.post("/orders",(req,res)=>{
+    res.send("<h1>A new order has been created</h1>")
+})
+app.get("/users",(req,res)=>{
+    res.send(`<h1>Here is the list of all users.</h1>`)
+})
+app.post("/users",(req,res)=>{
+    res.send(`<h1>A new user has been added</h1>`)
+})
+
 app.listen(port,()=>{
     console.log(`Server running on ${port}`);
     
 });
+
+
