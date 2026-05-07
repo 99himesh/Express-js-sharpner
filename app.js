7c7ef0aa5ac131979ed2136be8b2fc0fa55030f9
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-05-07 20:02:40 +0530

    file structure of router

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 const express=require("express");
 const app=express();
+const orderRoute=require("./router/order.js")
+const usersRoute=require("./router/users.js")
+const productsRoute=require("./router/products.js")
 let port=4000
 
-app.use((req,res,next)=>{
-    console.log("Authenticate middleware called");
-    next();
+// app.use((req,res,next)=>{
+//     console.log("Authenticate middleware called");
+//     next();
       
-})
+// })
 
-app.use("/library-2",(req,res,next)=>{
-    console.log("Book recomondation");
-    next()
-})
-app.use("/library-3",(req,res,next)=>{
-    console.log("Research Paper");
-    next()
-})
-app.use("/welcome",(req,res,next)=>{
-    req.user="Guest"
-    console.log("Welcome");
-    next();
-});
-
-
-app.use((req,res,next)=>{
-    console.log(`The request method is ${req.method} and url is ${req.url}`);
-    next();
-})
+// app.use("/library-2",(req,res,next)=>{
+//     console.log("Book recomondation");
+//     next()
+// })
+// app.use("/library-3",(req,res,next)=>{
+//     console.log("Research Paper");
+//     next()
+// })
+// app.use("/welcome",(req,res,next)=>{
+//     req.user="Guest"
+//     console.log("Welcome");
+//     next();
+// });
 
 
 
+
+// app.use((req,res,next)=>{
+//     console.log(`The request method is ${req.method} and url is ${req.url}`);
+//     next();
+// })
 // dynamic route 
 //params and query
 //dynmic route should be at top of the route in bottom is not working
@@ -40,42 +42,48 @@ app.get("/running/:username",(req,res)=>{
 })
 
 
+app.use("/orders",orderRoute)
+app.use("/users",usersRoute)
+app.use("/products",productsRoute)
 
 
-app.get("/library-2",(req,res)=>{
-    res.send("<h1>Library 2 called</h1>")
-})
-app.get("/library-3",(req,res)=>{
-    res.send("<h1>Library 3 called</h1>")
-})
-app.get("/Welcome",(req,res)=>{
-    res.send(`<h1>Welcome ${req.user}</h1>`)
-})
 
 
-app.get("/orders",(req,res)=>{
-    res.send("<h1>Here is the list of all orders.</h1>");
-})
 
-app.post("/orders",(req,res)=>{
-    res.send("<h1>A new order has been created</h1>");
-})
+// app.get("/library-2",(req,res)=>{
+//     res.send("<h1>Library 2 called</h1>")
+// })
+// app.get("/library-3",(req,res)=>{
+//     res.send("<h1>Library 3 called</h1>")
+// })
 
-app.get("/users",(req,res)=>{
-    res.send(`<h1>Here is the list of all users.</h1>`);
-})
 
-app.post("/users",(req,res)=>{
-    res.send(`<h1>A new user has been added</h1>`);
-})
+// app.get("/orders",(req,res)=>{
+//     res.send("<h1>Here is the list of all orders.</h1>");
+// })
 
-app.get("/products",(req,res)=>{
-    res.send("<h1>Here is the list of all products.</h1>");
-})
+// app.post("/orders",(req,res)=>{
+//     res.send("<h1>A new order has been created</h1>");
+// })
 
-app.post("/products",(req,res)=>{
-    res.send("<h1>A new product has been added.</h1>");
-})
+// app.get("/users",(req,res)=>{
+//     res.send(`<h1>Here is the list of all users.</h1>`);
+// })
+
+// app.post("/users",(req,res)=>{
+//     res.send(`<h1>A new user has been added</h1>`);
+// })
+
+
+
+// app.get("/products",(req,res)=>{
+//     res.send("<h1>Here is the list of all products.</h1>");
+// })
+
+// app.post("/products",(req,res)=>{
+//     res.send("<h1>A new product has been added.</h1>");
+// })
+
 
 app.get("/categories",(req,res)=>{
     res.send(`<h1>Here is the list of all categories.</h1>`);
@@ -83,6 +91,9 @@ app.get("/categories",(req,res)=>{
 
 app.post("/categories",(req,res)=>{
     res.send(`<h1>A new category has been created.</h1>`);
+})
+app.get("/Welcome",(req,res)=>{
+    res.send(`<h1>Welcome ${req.user}</h1>`)
 })
 
 app.use((req,res)=>{
